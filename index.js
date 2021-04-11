0d4d9ffe8a9e94811689846433e051865ab8da56
robsonmanata <70637849+robsonmanata@users.noreply.github.com>
2021-04-11 19:54:55 +0200

    server initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import postRoutes from './routes/posts.js';
+
+
+const app = express();
+
+app.use(express.json({limit:"30mb"}));
+app.use(express.urlencoded({limit:"30mb",extended:true}));
+app.use(cors());
+
+app.use('/posts',postRoutes);
+const CONNECTION_URL = "mongodb+srv://admin-robson:test123@cluster0.eo1nu.mongodb.net/myJournal?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
+.then(()=> app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
+.catch((error)=> console.log(error.message));
+
+mongoose.set('useFindAndModify',false);
