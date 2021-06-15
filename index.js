f6b7de83971e531540467a722523c486983b8e70
robsonmanata <70637849+robsonmanata@users.noreply.github.com>
2021-06-15 13:12:44 +0200

    iniatial commit3

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.use(cors());
 
 app.use('/posts',postRoutes);
 app.use("/user", userRouter);
+
+app.get('/',(req,res)=>{
+    res.send('Server is running');
+})
 const CONNECTION_URL = "mongodb+srv://admin-robson:test123@cluster0.eo1nu.mongodb.net/myJournal?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
 
