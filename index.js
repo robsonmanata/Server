8c2a31a6b6219e8705fb9952ef24d8eb144ef736
robsonmanata <70637849+robsonmanata@users.noreply.github.com>
2021-06-15 15:28:37 +0200

    env files

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import userRouter from './routes/user.js';
 
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({limit:"30mb"}));
 app.use(express.urlencoded({limit:"30mb",extended:true}));
@@ -20,10 +21,10 @@ app.use("/user", userRouter);
 app.get('/',(req,res)=>{
     res.send('Server is running');
 });
-const CONNECTION_URL = "mongodb+srv://admin-robson:test123@cluster0.eo1nu.mongodb.net/myJournal?retryWrites=true&w=majority"
+// const CONNECTION_URL = "mongodb+srv://admin-robson:test123@cluster0.eo1nu.mongodb.net/myJournal?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
 .then(()=> app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
 .catch((error)=> console.log(error.message));
 
